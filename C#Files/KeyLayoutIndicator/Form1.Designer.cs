2323f1d0d252d2c0fe1ed5af4a8597b30d07e0d1
Vsevolod Drebnev <tenodif@gmail.com>
2021-04-04 17:05:38 +0500

    Добавлен класс KeyboardLayoutTools, через него определяем раскладку аткивного окна. Пишем в порт и в лабели на форме через метод Invoke - так не вознкиает ошибок по вторжению процессов. Добавлена кнопка отключения от порта, и перескана портов

--- C#Files/KeyLayoutIndicator/Form1.Designer.cs
+++ C#Files/KeyLayoutIndicator/Form1.Designer.cs
@@ -33,13 +33,16 @@ namespace KeyLayoutIndicator
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.button1 = new System.Windows.Forms.Button();
+            this.BReScan = new System.Windows.Forms.Button();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.BDisconnect = new System.Windows.Forms.Button();
+            this.lLangStatus = new System.Windows.Forms.Label();
+            this.labelSost = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.BConnect = new System.Windows.Forms.Button();
             this.CBComs = new System.Windows.Forms.ComboBox();
             this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.lLangStatus = new System.Windows.Forms.Label();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -49,36 +52,101 @@ namespace KeyLayoutIndicator
             // 
             // groupBox1
             // 
+            this.groupBox1.Controls.Add(this.BReScan);
+            this.groupBox1.Controls.Add(this.BDisconnect);
             this.groupBox1.Controls.Add(this.lLangStatus);
-            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.labelSost);
             this.groupBox1.Controls.Add(this.label2);
             this.groupBox1.Controls.Add(this.label1);
-            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.BConnect);
             this.groupBox1.Controls.Add(this.CBComs);
             this.groupBox1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.groupBox1.Location = new System.Drawing.Point(0, 0);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(448, 216);
+            this.groupBox1.Size = new System.Drawing.Size(154, 148);
             this.groupBox1.TabIndex = 0;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Настройки";
             // 
-            // button1
+            // BReScan
             // 
-            this.button1.Location = new System.Drawing.Point(12, 46);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(121, 23);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "Подключится";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.BReScan.ImageIndex = 0;
+            this.BReScan.ImageList = this.imageList1;
+            this.BReScan.Location = new System.Drawing.Point(117, 19);
+            this.BReScan.Name = "BReScan";
+            this.BReScan.Size = new System.Drawing.Size(25, 23);
+            this.BReScan.TabIndex = 7;
+            this.BReScan.UseVisualStyleBackColor = true;
+            this.BReScan.Click += new System.EventHandler(this.BReScan_Click);
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "reload.png");
+            // 
+            // BDisconnect
+            // 
+            this.BDisconnect.Location = new System.Drawing.Point(12, 75);
+            this.BDisconnect.Name = "BDisconnect";
+            this.BDisconnect.Size = new System.Drawing.Size(130, 23);
+            this.BDisconnect.TabIndex = 6;
+            this.BDisconnect.Text = "Отключится";
+            this.BDisconnect.UseVisualStyleBackColor = true;
+            this.BDisconnect.Click += new System.EventHandler(this.BDisconnect_Click);
+            // 
+            // lLangStatus
+            // 
+            this.lLangStatus.AutoSize = true;
+            this.lLangStatus.Location = new System.Drawing.Point(79, 125);
+            this.lLangStatus.Name = "lLangStatus";
+            this.lLangStatus.Size = new System.Drawing.Size(63, 13);
+            this.lLangStatus.TabIndex = 5;
+            this.lLangStatus.Text = "lLangStatus";
+            // 
+            // labelSost
+            // 
+            this.labelSost.AutoSize = true;
+            this.labelSost.Location = new System.Drawing.Point(79, 103);
+            this.labelSost.Name = "labelSost";
+            this.labelSost.Size = new System.Drawing.Size(50, 13);
+            this.labelSost.TabIndex = 4;
+            this.labelSost.Text = "labelSost";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 125);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Язык:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 103);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Состояние:";
+            // 
+            // BConnect
+            // 
+            this.BConnect.Location = new System.Drawing.Point(12, 46);
+            this.BConnect.Name = "BConnect";
+            this.BConnect.Size = new System.Drawing.Size(130, 23);
+            this.BConnect.TabIndex = 1;
+            this.BConnect.Text = "Подключится";
+            this.BConnect.UseVisualStyleBackColor = true;
+            this.BConnect.Click += new System.EventHandler(this.BConnect_Click);
             // 
             // CBComs
             // 
             this.CBComs.FormattingEnabled = true;
             this.CBComs.Location = new System.Drawing.Point(12, 19);
             this.CBComs.Name = "CBComs";
-            this.CBComs.Size = new System.Drawing.Size(121, 21);
+            this.CBComs.Size = new System.Drawing.Size(99, 21);
             this.CBComs.TabIndex = 0;
             // 
             // notifyIcon1
@@ -88,50 +156,14 @@ namespace KeyLayoutIndicator
             this.notifyIcon1.Visible = true;
             this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
             // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 81);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(64, 13);
-            this.label1.TabIndex = 2;
-            this.label1.Text = "Состояние:";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(12, 103);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(38, 13);
-            this.label2.TabIndex = 3;
-            this.label2.Text = "Язык:";
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(82, 81);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(35, 13);
-            this.label3.TabIndex = 4;
-            this.label3.Text = "label3";
-            // 
-            // lLangStatus
-            // 
-            this.lLangStatus.AutoSize = true;
-            this.lLangStatus.Location = new System.Drawing.Point(82, 103);
-            this.lLangStatus.Name = "lLangStatus";
-            this.lLangStatus.Size = new System.Drawing.Size(35, 13);
-            this.lLangStatus.TabIndex = 5;
-            this.lLangStatus.Text = "label4";
-            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(448, 216);
+            this.ClientSize = new System.Drawing.Size(154, 148);
             this.Controls.Add(this.groupBox1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Индикатор раскладки";
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
@@ -143,12 +175,15 @@ namespace KeyLayoutIndicator
         private System.IO.Ports.SerialPort serialPort1;
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.ComboBox CBComs;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button BConnect;
         private System.Windows.Forms.NotifyIcon notifyIcon1;
         private System.Windows.Forms.Label lLangStatus;
-        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label labelSost;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button BDisconnect;
+        private System.Windows.Forms.Button BReScan;
+        private System.Windows.Forms.ImageList imageList1;
     }
 }
 
--- C#Files/KeyLayoutIndicator/Form1.cs
+++ C#Files/KeyLayoutIndicator/Form1.cs
@@ -9,33 +9,43 @@ using System.IO.Ports; //подключаем класс ком-порта
 using System.Threading;
 using System.Threading.Tasks; // подключаем для таймера
 using System.Windows.Forms;
+using System.Runtime.InteropServices; // подключаем для использования DllImport 
 
 namespace KeyLayoutIndicator
 {
     public partial class Form1 : Form
     {
-        bool timerEnable = false;
-        
+        // задаем переменные класса
+        SerialPort currentPort; // глобально объявляем используемы порт, т.к. будем обращаться из разных методов
+
+        //bool timerEnable = false;
+        //String CurrentLayout = "";
+        bool minimized = false;
+        System.Threading.Timer timer;
+        //String namePort = "";
+
+        //private delegate void updateDelegate(string txt);
 
 
-        public Form1()
+        public Form1() // метод создания формы
         {
             InitializeComponent();
-            // добавляем событие на изменение окна
-            this.Resize += new System.EventHandler(this.Form1_Resize); 
-            notifyIcon1.Visible = false;
+            this.Resize += new System.EventHandler(this.Form1_Resize);  // добавляем событие на изменение окна
+            notifyIcon1.Visible = false;    // приячем иконку в трее, пока программа не свернута
+            ComPortScan(); // сканируем список компортов и добавляем их в комбобокс
+        }
 
-            //CBComs.Items.Clear(); чистим список COM портов 
-            // Получаем список COM портов доступных в системе
-            string[] portnames = SerialPort.GetPortNames();
-            // Проверяем есть ли доступные
+        void ComPortScan()
+        {
+            CBComs.Items.Clear(); //чистим список COM портов - необходимо, когда вызывается на кнопке "обновить список"            
+            string[] portnames = SerialPort.GetPortNames(); // Получаем список COM портов доступных в системе
+            // Проверяем есть ли доступные порты, если нет, выводим сообщение что не найдены
             if (portnames.Length == 0)
             {
                 MessageBox.Show("COM PORT not found");
             }
-            foreach (string portName in portnames)
+            foreach (string portName in portnames) // прочесываем массив с именами компортов и добавляем их в комбобокс
             {
-                //добавляем доступные COM порты в список           
                 CBComs.Items.Add(portName);
                 Console.WriteLine(portnames.Length);
                 if (portnames[0] != null)
@@ -43,23 +53,59 @@ namespace KeyLayoutIndicator
                     CBComs.SelectedItem = portnames[0];
                 }
             }
-            System.Threading.Timer tim2 = new System.Threading.Timer(TimerTick, null, 1000, 500);
+        }
+        private void BReScan_Click(object sender, EventArgs e)
+        {
+            ComPortScan();
+        }
+        private void BConnect_Click(object sender, EventArgs e) // нажатие кнопки "подключить"
+        {
+            currentPort = new SerialPort(CBComs.Text, 9600);
+            currentPort.DtrEnable = true;
+            currentPort.Open();
+            //serialPort1.Close();
+            //serialPort1.PortName = CBComs.Text;
+            //namePort = CBComs.Text;
+            //serialPort1.Open();
+            //timerEnable = true;
+            labelSost.Text = "Подключен";
+            timer = new System.Threading.Timer(TimerTick, null, 1000, 300); // создаем таймер
+            BConnect.Enabled = false; // делаем кнопмку подключения неактивной
+        }
 
-
-
+        private void BDisconnect_Click(object sender, EventArgs e)
+        {
+            currentPort.Close();
+            labelSost.Text = "Отключен";
+            BConnect.Enabled = true; // делаем кнопмку подключения неактивной
+        }
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            // проверяем наше окно, и если оно было свернуто, делаем событие        
+            if (WindowState == FormWindowState.Minimized)
+            {
+                this.ShowInTaskbar = false; // прячем наше окно из панели
+                notifyIcon1.Visible = true; // делаем нашу иконку в трее активной
+                minimized = true;
+            }
+        }
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e) // по двойному щелчку разворачиваем окно 
+        {
+            notifyIcon1.Visible = false;    // делаем нашу иконку скрытой
+            this.ShowInTaskbar = true;  // возвращаем отображение окна в панели
+            WindowState = FormWindowState.Normal;   //разворачиваем окно
+            minimized = false;
         }
 
         void TimerTick(object sender)
         {
-             getLayout();
-            
+            if (minimized) this.Invoke(new Action(() => GetLayOut())); //вызов из свернутого состояния, для определения раскладки другого окна
+            else this.Invoke(new Action(() => GetLayOutInForm()));// вызов, если активно окно нашей программы
+            // используется Invoke из-за того, что форма и объекты из кода рождаются в разных процессах, Invoke нужен для доступа одногопроцесса в другой
+
         }
-
-
-
-        private void getLayout()
+        void GetLayOutInForm()
         {
-            if (!timerEnable) return;
             InputLanguage myCurrentLanguage = InputLanguage.CurrentInputLanguage;
             if (myCurrentLanguage != null)
             {
@@ -68,67 +114,121 @@ namespace KeyLayoutIndicator
                 if (txt == "Русская")
                 {
                     PortWrite("1");
+                    lLangStatus.Text = "RU";
                 }
                 if (txt == "США")
                 {
                     PortWrite("3");
+                    lLangStatus.Text = "EN";
                 }
-                lLangStatus.Text = txt;
-
-
             }
         }
+        void GetLayOut() // метод определения раскладки активного окна 
+        {
+            IntPtr selectedWindow = KeyboardLayoutTools.NativeMethods.GetForegroundWindow();
+            int currId;
+            if (KeyboardLayoutTools.CheckKeyboardLayout(Handle, selectedWindow, out currId))
+            {
+
+                switch (currId)
+                {
+                    case 1033:  //английский
+                        PortWrite("1");
+                        break;
+                    case 1049:  //русский
+                        PortWrite("3");
+                        break;
+                }
+            }
+        }
+
+
 
         void PortWrite(String txt) //функция записи переданного параметра в порт
         {
             try
             {
-                //установить проверку, сменился ли язык, что бы не спамить ком порт
-                //serialPort1.Open();
-                //lComStatus.Text = "Работает";
-                //System.Threading.Thread.Sleep(100);
-                // небольшая пауза, ведь SerialPort не терпит суеты
-                serialPort1.Write(txt);
-                //serialPort1.Close();
-                
+                System.Threading.Thread.Sleep(100); // небольшая пауза, ведь SerialPort не терпит суеты
+                currentPort.Write(txt);
             }
             catch
             {
-                //lComStatus.Text = "Ошибка открытия порта";
+                //if (!minimized) labelSost.Text = "Ошибка открытия порта";
             }
 
         }
 
-       
-        private void button1_Click(object sender, EventArgs e)
+
+    }
+    public class KeyboardLayoutTools
+    {
+
+        private static readonly InputLanguageCollection InstalledInputLanguages = InputLanguage.InstalledInputLanguages;
+        private static int _lastKeyLayout = -1;
+
+
+        public static int GetKeyboardLayoutId(IntPtr windowHandle)
         {
-            serialPort1.Close();
-            serialPort1.PortName = CBComs.Text;
-            serialPort1.Open();
-            timerEnable = true;
-            //tm.Start();
+            int lpdwProcessId;
+            int winThreadProcId = NativeMethods.GetWindowThreadProcessId(windowHandle, out lpdwProcessId);
+            IntPtr keybLayout = NativeMethods.GetKeyboardLayout(winThreadProcId);
+
+            for (int i = 0; i < InstalledInputLanguages.Count; i++)
+                if (keybLayout == InstalledInputLanguages[i].Handle)
+                    return InstalledInputLanguages[i].Culture.KeyboardLayoutId;
+
+            return -1;
         }
 
-        private void Form1_Resize(object sender, EventArgs e)
+        public static bool CheckKeyboardLayout(IntPtr thisWindow, IntPtr selectedWindow, out int currentLayId)
         {
-            // проверяем наше окно, и если оно было свернуто, делаем событие        
-            if (WindowState == FormWindowState.Minimized)
+            currentLayId = _lastKeyLayout;
+            if (selectedWindow != thisWindow)
             {
-                // прячем наше окно из панели
-                this.ShowInTaskbar = false;
-                // делаем нашу иконку в трее активной
-                notifyIcon1.Visible = true;
+                int foregroundWindowInput = GetKeyboardLayoutId(selectedWindow);
+                if (foregroundWindowInput == _lastKeyLayout) return false;
+
+                int thisWindowInput = GetKeyboardLayoutId(thisWindow);
+                currentLayId = _lastKeyLayout = thisWindowInput;
+
+                if (thisWindowInput != foregroundWindowInput)
+                {
+                    NativeMethods.ActivateKeyboardLayout(new IntPtr(1), 8);
+                    return true;
+                }
             }
+            return false;
         }
 
-        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        #region Nested type: NativeMethods
+
+        public static class NativeMethods
         {
-            // делаем нашу иконку скрытой
-            notifyIcon1.Visible = false;
-            // возвращаем отображение окна в панели
-            this.ShowInTaskbar = true;
-            //разворачиваем окно
-            WindowState = FormWindowState.Normal;
+            /// 
+            /// Получает маркер активного окна Windows.
+            /// 
+            [DllImport("user32.dll", CharSet = CharSet.Auto, ExactSpelling = true)]
+            public static extern IntPtr GetForegroundWindow();
+
+            /// 
+            /// Получает идентификационный номер потока для окна.
+            /// 
+            [DllImport("user32.dll", CharSet = CharSet.Auto, ExactSpelling = true)]
+            public static extern int GetWindowThreadProcessId(IntPtr handleWindow, out int lpdwProcessId);
+
+            /// 
+            /// Получает информацию о раскладке клавиатуры.
+            /// 
+            [DllImport("user32.dll", CharSet = CharSet.Auto, ExactSpelling = true)]
+            public static extern IntPtr GetKeyboardLayout(int windowsThreadProcessId);
+
+            /// 
+            /// Активирует раскладку клавиатуры.
+            /// 
+            [DllImport("user32.dll")]
+            public static extern int ActivateKeyboardLayout(IntPtr nkl, uint flags);
         }
+
+        #endregion
     }
 }
